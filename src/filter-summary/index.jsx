f664a5deaefae51e0ac9cd3af4968eb2d84fe007
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2019-07-30 11:21:32 +0100

    add a button to remove search filter

--- src/filter-summary/index.jsx
+++ src/filter-summary/index.jsx
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 
 const FilterSummary = ({
   total,
   filtered,
-  filteredLabel = `Showing ${filtered} of ${total} results`,
-  allShowingLabel = `All ${total} results`
-}) => (
-  <h2 className="filter-summary">
-    {
-      filtered !== total
-        ? filteredLabel
-        : allShowingLabel
-    }
-  </h2>
-);
+  filteredLabel = filtered === 1 ? `${filtered} result` : `${filtered} results`,
+  allShowingLabel = total === 1 ? `${total} result` : `${total} results`,
+  filters
+}) => {
+  const searchString = filters.active['*'];
+
+  const removeFilter = e => {
+    e.preventDefault();
+    window.location.href = `//${window.location.host + window.location.pathname}`;
+  };
+
+  return (
+    <Fragment>
+      <h3 className="filter-summary">
+        { filtered !== total ? filteredLabel : allShowingLabel }
+      </h3>
+      {
+        searchString &&
+          <div className="applied-filters">
+            <h3>Containing</h3>
+            <button className="govuk-button button-secondary search-filter" onClick={removeFilter}>
+              <span>{searchString}</span>
+            </button>
+          </div>
+      }
+    </Fragment>
+  );
+};
 
 const mapStateToProps = ({
   datatable: {
     pagination: {
       count: filtered,
       totalCount: total
-    }
+    },
+    filters
   }
 }) => ({
-  total,
-  filtered
+  total: parseInt(total),
+  filtered: parseInt(filtered),
+  filters
 });
 
 export default connect(
