fd0e8290dd42f569f4e688b836b64396b1f757fb
Kiran Varma <Kiran.Varma@digital.homeoffice.gov.uk>
2024-11-21 11:43:00 +0000

    fix tests

--- src/word-count/index.spec.jsx
+++ src/word-count/index.spec.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import WordCount from '.';
+import WordCountHintMessage from './wordcount-hint-message';
 import { describe, test, expect } from '@jest/globals';
 
-describe('<WordCount />', () => {
+describe('<WordCountHintMessage />', () => {
 
     const values = { id: 'applicantTrainingUseAtWork' };
 
     test('displays how many words remaining', () => {
-        const wrapper = shallow(<WordCount value="Hello world" maxWordCount={10} values={values} />);
+        const wrapper = shallow(<WordCountHintMessage wordCount={8} maxWordCount={10} values={values} />);
 
-        expect(wrapper.find('#applicantTrainingUseAtWork-wordcount-hint').text()).toContain('You have 8 words remaining');
+        expect(wrapper.find('#applicantTrainingUseAtWork-wordcount-hint').text()).toContain('You have 2 words remaining');
     });
 
     test('displays maximum word count when textarea is empty', () => {
-        const wrapper = shallow(<WordCount value="" maxWordCount={10} values={values}/>);
+        const wrapper = shallow(<WordCountHintMessage wordCount={0} maxWordCount={10} values={values}/>);
 
         expect(wrapper.find('#applicantTrainingUseAtWork-wordcount-hint').text()).toContain('You have 10 words remaining');
     });
 
     test('displays how many words above the maximum word limit are entered', () => {
-        const wrapper = shallow(<WordCount value="Hello world" maxWordCount={1} values={values}/>);
+        const wrapper = shallow(<WordCountHintMessage wordCount={8} maxWordCount={1} values={values}/>);
 
-        expect(wrapper.find('#applicantTrainingUseAtWork-wordcount-hint').text()).toContain('You have 1 word too many');
+        expect(wrapper.find('#applicantTrainingUseAtWork-wordcount-hint').text()).toContain('You have 7 words too many');
     });
 });
