19e3f71cd34acbd484a50436d223eefe5ce7cf22
kiran-varma-home-office <kiran.varma@digital.homeoffice.gov.uk>
2025-02-06 21:45:59 +0000

    add jsdoc

--- src/supporting-links/index.jsx
+++ src/supporting-links/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+/**
+ * SupportingLinks component renders a section with a title and a list of links.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.sectionTitle - The title of the section.
+ * @param {Array} props.links - An array of link objects.
+ * @param {string} props.links[].href - The URL of the link.
+ * @param {string} props.links[].label - The label of the link.
+ *
+ * @returns {JSX.Element} The rendered SupportingLinks component.
+ */
 const SupportingLinks = ({ sectionTitle, links }) => (
     <div className="govuk-grid-column-one-third">
         <div className="x-govuk-related-navigation">
